ae85d5f636c6f1517f374d29e042a3420097c07a
Ben Remy <bremy@vassar.edu>
2024-04-12 18:48:23 +0800

    memory

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,10 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+//import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from "../assets/img/wave.png";
 //import 'animate.css';
 //import TrackVisibility from 'react-on-screen';
 
+//, backgroundColor: 'white', padding: '10px', borderRadius: '5px'
 export const Banner = () => {
   
     
@@ -15,12 +16,12 @@ export const Banner = () => {
                     <Col xs={12} ms={6} xl={7}>
                             <div>
                                 <span className="tagline">Welcome to my Portfolio!</span>
-                                <h1 style={{ color: 'black', backgroundColor: 'white', padding: '10px', borderRadius: '5px' }}>{"Hi, I'm Benjamin "}<span className="wrap"></span></h1>
-                                <p style={{ color: 'black', backgroundColor: 'white', padding: '10px', borderRadius: '5px' }}>
+                                <h1 style={{ color: 'black'}}>{"Hi, I'm Benjamin "}<span className="wrap"></span></h1>
+                                <p style={{ color: 'black'}}>
                                     I am a Junior at Vassar College studying Computer Science and Mathematics. I also 
                                     studied for a semester at Yale NUS college in Singapore. Thanks for taking a look at
                                     my portfolio. Please feel free to check out my <a href="/#projects">projects</a> and <a href="/#contacts">contact</a> me if you have any questions.</p>
-                                <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25} /></button>
+
                             </div>
                     </Col>
                     <Col xs={12} ms={6} xl={5}>
