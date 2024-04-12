ec5ba7be379939c144aa828a254ce22703faec9c
Ben Remy <bremy@vassar.edu>
2024-04-12 18:58:42 +0800

    memory

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -25,7 +25,7 @@ export const ContactForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch('http://localhost:5000/contact', {
+        let response = await fetch('/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;chatset=utf-8'
